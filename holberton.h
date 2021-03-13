46de04482e2e1482a5a9996dcf7b4f42564136a5
pgomezboza <pgomezboza@gmail.com>
2021-03-13 10:10:56 -0500

    update file holberton.h

--- holberton.h
+++ holberton.h
@@ -1,6 +1,19 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+typedef struct type
+{
+        char *id_type;
+        int (*function)(va_list);
+} print_type;
+
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list c);
+int print_string(va_list s);
 
 #endif /* _HOLBERTON_H_ */
