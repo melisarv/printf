6f731aae738ba96b042f9ffc0334e9e6e4f73ae4
Melisa Rojas <melisarv@gmail.com>
2021-03-15 13:11:32 -0500

    create function that prints

---
+++ _printf.c
@@ -0,0 +1,75 @@
+#include "holberton.h"
+#include <stdarg.h>
+
+/**
+ * get_functions - select the correct function
+ * @format: character string containing directives
+ *
+ * Return:  pointer to the function
+ */
+int (*get_functions(const char *format))(va_list)
+{
+	unsigned int i = 0;
+
+	print_type print[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	while (print[i].id_type != NULL && *(print[i].id_type) != *format)
+	{
+		i++;
+	}
+	return (print[i].function);
+}
+
+
+/**
+ * _printf - prints according to the format entered
+ * @format: character string containing directives
+ *
+ * Return: number of character printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list args;
+
+	int (*function)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
+
+		if (!format[i])
+			return (count);
+
+		function = get_functions(&format[i + 1]);
+
+		if (function != NULL)
+		{
+			count += function(args);
+			i += 2;
+			continue;
+		}
+
+		if (!format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(args);
+	return (count);
+}
