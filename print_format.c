997c03b51194b02828c60c436e3add902bce4099
Melisa Rojas <melisarv@gmail.com>
2021-03-15 16:49:52 -0500

    add function print_number

--- print_format.c
+++ print_format.c
@@ -44,3 +44,49 @@ int print_string(va_list s)
 	return (i);
 }
 
+/**
+* print_number - prints a number from a va_list.
+*
+*@i: number to print.
+*
+*Return: result
+*/
+
+int print_number(va_list i)
+{
+	int cont = 0, div, n, num;
+
+	n = va_arg(i, int);
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			_putchar('-');
+		}
+		num = n;
+		while (num != 0)
+		{
+			num = num / 10;
+			cont++;
+		}
+		num = 1;
+		div = 1;
+		while (num < cont)
+		{
+			div = div * 10;
+			num++;
+		}
+		num = 1;
+		while (num <= cont)
+		{
+			_putchar((n / div) % 10 + '0');
+			div = div / 10;
+			num++;
+		}
+	}
+	return (0);
+}
