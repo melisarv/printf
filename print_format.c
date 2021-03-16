2184a55ee8f815aaf66297c9ad3971c52733627b
Melisa Rojas <melisarv@gmail.com>
2021-03-16 11:28:59 -0500

    function update

--- print_format.c
+++ print_format.c
@@ -7,7 +7,6 @@
 *
 *Return: 1 on success.
 */
-
 int print_char(va_list args)
 {
 	char chtr;
@@ -18,7 +17,6 @@ int print_char(va_list args)
 
 	return (1);
 }
-
 /**
 * print_string - print a string from a va_list.
 *
@@ -57,39 +55,39 @@ int print_number(va_list args)
 	int cont = 0, div, n, num;
 
 	n = va_arg(args, int);
-
 	if (n == 0)
+	{
 		_putchar('0');
+		cont++;
+	}
 	else
 	{
 		if (n < 0)
-	{
+		{
 			_putchar('-');
-	n *= -1;
-	}
-
-	num = n;
-	while (num != 0)
-	{
-		num /= 10;
-		cont++;
-	}
-
-	num = 1;
-	div = 1;
-	while (num < cont)
-	{
-		div *= 10;
-		num++;
-	}
-
-	num = 1;
-	while (num <= cont)
-	{
-		_putchar((n / div) % 10 + '0');
-		div = div / 10;
-		num++;
-	}
+			n *= -1;
+			cont++;
+		}
+		num = n;
+		while (num != 0)
+		{
+			num /= 10;
+			cont++;
+		}
+		num = 1;
+		div = 1;
+		while (num < cont)
+		{
+			div *= 10;
+			num++;
+		}
+		num = 1;
+		while (num <= cont)
+		{
+			_putchar((n / div) % 10 + '0');
+			div = div / 10;
+			num++;
+		}
 	}
 	return (cont);
 }
